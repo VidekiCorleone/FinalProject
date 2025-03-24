ee7e533fc34b1d966a3f6ac8e971d1263d418d2d
Zsiros Máté <zsiros.mate19k@egressy.info>
2025-03-24 11:41:38 +0100

    asd

--- asztali/project_alpha_0.1/project_alpha_0.1/signIn.cs
+++ asztali/project_alpha_0.1/project_alpha_0.1/signIn.cs
@@ -51,10 +51,10 @@ namespace project_alpha_0._1
             var user = textBox1.Text.ToLower();
             var pass = textBox2.Text.ToLower();
 
-            /*using (HttpClient client = new HttpClient())
+            using (HttpClient client = new HttpClient())
             {
                 var content = new StringContent(JsonConvert.SerializeObject(new { username = user, password = pass }), Encoding.UTF8, "application/json");
-                var response = await client.PostAsync("http://localhost:3000/login", content);
+                var response = await client.PostAsync("http://127.1.1.1:3000/login", content);
                 var responseString = await response.Content.ReadAsStringAsync();
                 var result = JsonConvert.DeserializeObject<dynamic>(responseString);
 
@@ -71,10 +71,10 @@ namespace project_alpha_0._1
                     const string caption = "Helytelen adat";
                     MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
-            }*/
+            }
 
 
-            if (user == "admin" && pass == "admin")
+            /*if (user == "admin" && pass == "admin")
             {
                 this.Hide();
                 Menu menu = new Menu();
@@ -86,7 +86,7 @@ namespace project_alpha_0._1
                 const string message = "Helytelen felhasználónév vagy jelszó!";
                 const string caption = "Helytelen adat";
                 MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+            }*/
         }
 
         public void closeFunc(object s, EventArgs e)
