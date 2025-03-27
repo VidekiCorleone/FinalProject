7234b4f828010d5e934be9df5e882910289ac150
Zsiros Mate <zsiros.mate19k@egressy.info>
2025-03-27 05:29:36 +0100

    WinForm server login attempt#1

---
+++ asztali/project_alpha_0.1/project_alpha_0.1/osztalyok/BejelentkezesiAdatok.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_alpha_0._1.osztalyok
+{
+    public class BejelentkezesiAdatok
+    {
+        public string uName { get; set; }
+        public string uPass { get; set; }
+    }
+}
---
+++ asztali/project_alpha_0.1/project_alpha_0.1/osztalyok/TokenValasz.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_alpha_0._1.osztalyok
+{
+    public class TokenValasz
+    {
+        public string Token { get; set; }
+    }
+}
